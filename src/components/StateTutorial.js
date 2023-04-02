e962fa71eb38fb4b22843d7138657a36f1ff824a
ocjarman <ocjarman@gmail.com>
2023-04-02 12:32:37 -0400

    usestate review

--- src/components/StateTutorial.js
+++ src/components/StateTutorial.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { useState } from "react";
 
 const StateTutorial = () => {
-  return <div>Hello World</div>;
+  const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState("this is my input");
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const changeInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>State testing!</h1>
+      <hr></hr>
+      <div>
+        Counter: {counter}
+        <button onClick={increment}>Increment</button>
+      </div>
+      <hr></hr>
+      <div>
+        Input: {input}
+        <br></br>
+        <input placeholder="enter something..." onChange={changeInput} />
+      </div>
+      <hr></hr>
+    </div>
+  );
 };
 
 export default StateTutorial;
