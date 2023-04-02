947127cffbefd10b27a40379dd0c910a97c9e463
ocjarman <ocjarman@gmail.com>
2023-04-02 12:52:11 -0400

    installed axios and created useeffect component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import StateTutorial from "./components/StateTutorial";
 import ReducerTutorial from "./components/ReducerTutorial";
+import EffectTutorial from "./components/EffectTutorial";
+
 function App() {
   return (
     <div className="App">
       <StateTutorial />
       <ReducerTutorial />
+      <EffectTutorial />
     </div>
   );
 }
---
+++ src/components/EffectTutorial.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useEffect } from "react";
+
+const EffectTutorial = () => {
+  return (
+    <div>
+      <h1>useEffect testing!</h1>
+      <hr></hr>
+      <div></div>
+      <hr></hr>
+      <hr></hr>
+    </div>
+  );
+};
+
+export default EffectTutorial;
